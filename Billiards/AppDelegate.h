3d8a4f5d302f47891af0272488d25d4db29cfdbf
Gregory Casamento <greg.casamento@gmail.com>
2026-05-23 13:01:50 -0400

    Initial Commit

---
+++ Billiards/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Billiards
+//
+//  Created by Gregory Casamento on 5/23/26.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
